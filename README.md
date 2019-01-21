275bd8100bfdc0e9b2ebb8cbf4080fb6ae62c9ce
znti <znti.rafael@gmail.com>
2019-01-21 09:42:14 -0200

    Set file structure and basic dojot-web usage

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dojot-cli
 Command line tool to interact with the dojot plataform
+
+# Installing
+This module is supposed to be used directly through command line, hence its recommended to install it globally
+
+`npm install -g @znti/dojot-cli`
+
+# Usage
+After installation is complete, you can test the module with 
+
+`dojot hello-world`
---
+++ index.js
@@ -0,0 +1,59 @@
+let dojot = require('@znti/dojot-web');
+
+// Sets the base point for dojot.
+let dojotHost = 'http://localhost:8000';
+
+// This parameter on init() is actually optional. 
+// If nothing is passed, admin/admin is assumed
+// The only reason its here is to guide users in need of using custom credentials
+let credentials = {username: 'admin', passwd: 'admin'}
+
+//First two arguments passed are node and .js file, so we skip them
+let args = process.argv;
+args.splice(0, 2);
+
+let [command, resource, data] = args;
+
+dojot.init(dojotHost, credentials).then(dojotClient => {
+	let {Templates, Devices} = dojotClient;
+	switch(command.toLowerCase()) {
+		case 'hello-world':
+			helloWorld(Templates, Devices);
+			break;
+		case 'powerwash':
+			powerwash(Templates, Devices);
+			break;
+		default:
+			console.log(`Unknown command ${command}. Nothing to do.`);
+	}
+});
+
+helloWorld = async (Templates, Devices) => {
+	console.log('Dojot client started correctly. Loading data..');
+	let devices = await Devices.get() || [];
+	let templates = await Templates.get() || [];
+
+	console.log(`Got ${devices.length} devices and ${templates.length} templates`);
+	console.log('Everything seems to be working as expected');
+}
+powerwash = async (Templates, Devices) => {
+	console.log('Loading data references..');
+	let devices = await Devices.get() || [];
+	let templates = await Templates.get() || [];
+
+	console.log(`Got ${devices.length} devices and ${templates.length} templates`);
+	console.log('Cleaning things up');
+
+	await devices.map(d => Devices.delete(d));
+	console.log('Devices were deleted');
+
+	// Apparently device-manager needs a little time to breathe
+	await sleep(1000);
+
+	await templates.map(t => Templates.delete(t));
+	console.log('Templates were deleted');
+}
+
+sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
